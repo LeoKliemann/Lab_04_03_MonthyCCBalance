43dc30c3d2dbcaf5b5aa851b431a4c204ebbd86e
LeoKliemann <lgkliemann05@gmail.com>
2026-02-06 16:47:31 -0500

    Finished task 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ public class Main{
     public static void main(String[] args)
     {
         //Declarations
-        double creditCarBalance = 5000;
+        double creditCardBalance = 5000;
         double interestRate = .17;
+        double balanceTotal;
         double firstMonth;
         double secondMonth;
+
+        //Statements
+        balanceTotal = interestRate * creditCardBalance;
+        firstMonth = balanceTotal + creditCardBalance;
+        secondMonth = firstMonth + balanceTotal;
+        System.out.println("Your balance for the first month is: $" + firstMonth + ", and your balance for the second month is, $" + secondMonth);
 
     }
 }
