e2e4b82d1693623f5b1a6b3839f66b1ee7aed7ad
LeoKliemann <lgkliemann05@gmail.com>
2026-02-06 16:40:49 -0500

    Declarations done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String[] args)
     {
-        
+        //Declarations
+        double creditCarBalance = 5000;
+        double interestRate = .17;
+        double firstMonth;
+        double secondMonth;
+
     }
 }
